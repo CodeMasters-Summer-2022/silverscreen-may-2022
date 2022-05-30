9b949b18b3072d30c0ce89156cb87beaf38ebe1b
Deepak Joy Jose <deejayjay@zoho.com>
2022-05-30 12:45:45 -0600

    Adds global styles for the Top Bar and Side Nav.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 console.log("Hello from JavaScript");
 const apiKey = process.env.SILVERSCREEN_TMDB_API_KEY;
 console.log({ apiKey });
+
+const menuBtn = document.querySelector(".menu-btn");
+const sideNav = document.querySelector(".side-nav");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  sideNav.classList.toggle("side-nav__open");
+});
