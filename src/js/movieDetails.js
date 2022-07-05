9569101f61984f1fcfb93274a408e6d72935d7bb
nnabighods <n.nabighods@gmail.com>
2022-07-04 19:21:16 -0600

    movieID

--- src/js/movieDetails.js
+++ src/js/movieDetails.js
@@ -3,9 +3,5 @@
 // id, title, overview, poster_path, release_date, release_date, budget, revenue, runtime,
 // tagline, vote_average
 
-function getMovieDetails(){
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const movieID = urlParams.get('movieId')
-    console.log(movieID);
-}
+const param = new URLSearchParams(window.location.search);
+movieID = param.get('movieId');
