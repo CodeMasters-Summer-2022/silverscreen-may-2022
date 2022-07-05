7f7e6bf971e641aff4944a3e6f8da3ef8b3c9c8e
nnabighods <n.nabighods@gmail.com>
2022-07-04 18:37:13 -0600

    URLparam

--- src/js/movieDetails.js
+++ src/js/movieDetails.js
@@ -2,3 +2,10 @@
 // MovieDb API GET Movie Details endpoint:
 // id, title, overview, poster_path, release_date, release_date, budget, revenue, runtime,
 // tagline, vote_average
+
+function getMovieDetails(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const movieID = urlParams.get('movieId')
+    console.log(movieID);
+}
